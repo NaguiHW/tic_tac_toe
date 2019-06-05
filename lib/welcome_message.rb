73585c1fae48095b6a9592ed0e90cd456e302848
Kalib Hackin <nagui08@hotmail.com>
2019-06-05 11:52:19 -0500

    fixing some errors

---
+++ lib/welcome_message.rb
@@ -0,0 +1,5 @@
+def welcome_message
+    p "Welcome to Tic Tac Toe Game"
+    p "Created bu Bernardo Garza and Kalib Hackin"
+    p "Before play the game, read the README.md file to know the instructions"
+end
