71709fbd0bc7c61d39d792571370b971f5a31a7d
Kalib Hackin <nagui08@hotmail.com>
2019-06-05 13:44:38 -0500

    players position choice

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,11 @@
 load "../lib/create_player.rb"
 load "../lib/board.rb"
 
+$ar = (1..9).to_a
+def board ar
+    puts "\n #{ar[0]} | #{ar[1]} | #{ar[2]} \n---+---+--- \n #{ar[3]} | #{ar[4]} | #{ar[5]} \n---+---+--- \n #{ar[6]} | #{ar[7]} | #{ar[8]}\n \n"
+end
+
 def mark_selection
     p "Player 1, what is your name?"
     $player1 = CreatePlayer.new
@@ -12,28 +17,28 @@ def mark_selection
     $player2.name = gets.chomp
     $player1.mark == 'X' ? $player2.mark = 'O' : $player2.mark = 'X'
     p "#{$player2.name}, your mark is #{$player2.mark}"
-    $player1
+    board $ar
 end
 
-def board
-    ar = (1..9).to_a
-    puts "\n #{ar[0]} | #{ar[1]} | #{ar[2]} \n---+---+--- \n #{ar[3]} | #{ar[4]} | #{ar[5]} \n---+---+--- \n #{ar[6]} | #{ar[7]} | #{ar[8]}\n"
+def player_turn player
+    p "#{player.name}, where do you want to put your mark? (#{player.mark})"
+    player1_selection = gets.chomp.to_i
+    until $ar.include?(player1_selection) 
+        p "Enter a correct number"
+        player1_selection = gets.chomp.to_i
+    end
+    $ar[player1_selection-1] = player.mark
 end
 
 def selection
     turns = 1
     while turns <= 9
-        board
         if turns % 2 == 1
-            p "#{$player1.name}, where do you want to put your mark? (#{$player1.mark})"
-            player1_selection = gets.chomp
-            ar[player1_selection-1] = "X"
+            player_turn $player1
         else
-            player = $player2
-            p "#{$player2.name}, where do you want to put your mark? (#{$player2.mark})"
-            player2_selection = gets.chomp
-            ar[player2_selection-1] = "O"
+            player_turn $player2
         end
         turns += 1
+        board $ar
     end
 end
