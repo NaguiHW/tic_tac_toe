e5c062240041de4879fce2bfdfc80ba523de8580
Kalib Hackin <nagui08@hotmail.com>
2019-06-07 12:09:32 -0500

    trying to call the first method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,10 +2,28 @@ require './lib/game.rb'
 
 describe Game do
 
-    describe '#who_wins' do
+    class CreatePlayer
+        attr_accessor :name, :mark
+        def initialize
+          @player_ar = []
+        end
+        attr_reader :player_ar
+    end
+
+    player = CreatePlayer.new
+    player.name = 'K'
+    player.player_ar << 1
+    player.player_ar << 2
+    player.player_ar << 3
+
+    def answear(player)
+        p "#{player.name} is the winner"
+        $turns = 9
+    end
+
+    describe '#who_wins(player)' do
         it 'if we have one of a kind in a row then it should return the player who wins' do
-            arr=[1, 2, 3]
-            expect().to eql(true)
+            expect(who_wins(player)).to eql("K is the winner", 9)
         end
         
     end
--- test.rb
+++ test.rb
@@ -1,3 +1,43 @@
-arr = (1..6).to_a
+class CreatePlayer
+    attr_accessor :name, :mark
+    def initialize
+      @player_ar = []
+    end
+    attr_reader :player_ar
+end
 
-arr.each { |x| p x * 2 }
+player = CreatePlayer.new
+player.name = 'K'
+player.player_ar << 1
+player.player_ar << 2
+player.player_ar << 3
+
+def who_wins(player)
+    if player.player_ar.include?(1) && player.player_ar.include?(2) && player.player_ar.include?(3)
+      answear player
+    elsif player.player_ar.include?(4) && player.player_ar.include?(5) && player.player_ar.include?(6)
+      answear player
+    elsif player.player_ar.include?(7) && player.player_ar.include?(8) && player.player_ar.include?(9)
+      answear player
+    elsif player.player_ar.include?(1) && player.player_ar.include?(4) && player.player_ar.include?(7)
+      answear player
+    elsif player.player_ar.include?(2) && player.player_ar.include?(5) && player.player_ar.include?(8)
+      answear player
+    elsif player.player_ar.include?(3) && player.player_ar.include?(6) && player.player_ar.include?(9)
+      answear player
+    elsif player.player_ar.include?(1) && player.player_ar.include?(5) && player.player_ar.include?(9)
+      answear player
+    elsif player.player_ar.include?(3) && player.player_ar.include?(5) && player.player_ar.include?(7)
+      answear player
+    else
+      p 'Nobody wins'
+    end
+  
+end 
+
+def answear(player)
+  p "#{player.name} is the winner"
+  $turns = 9
+end
+
+p who_wins(player)
